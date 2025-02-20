a1702e8219235e5b2fe4e7214e456e74173b0db8
Yash Patil <stud22.ysp1@gec.ac.in>
2025-02-20 17:36:26 +0530

    edited

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"]
 
 
 
-@app.route('/',methods=['POST'])
+@app.route('/',methods=['GET'])
 def home():
     return jsonify({"message":"Hello World","status":200})
 
